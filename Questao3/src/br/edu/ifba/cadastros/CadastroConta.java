2b2d56fa081bd6e0aca8c07ed4535e5e64fd3de0
IgorCosta <94564085+igorcostaif@users.noreply.github.com>
2023-03-30 12:22:50 -0300

    Add files via upload

--- Questao3/src/br/edu/ifba/cadastros/CadastroConta.java
+++ Questao3/src/br/edu/ifba/cadastros/CadastroConta.java
@@ -6,7 +6,7 @@ import br.edu.ifba.basica.Conta;
 
 public class CadastroConta {
 
-	Conta[] conta = new Conta[2];
+	Conta[] conta = new Conta[5];
 	int total = 0;
 
 	public void adicionar(Conta c) {
@@ -14,14 +14,56 @@ public class CadastroConta {
 		this.total++;
 	}
 
+	public void adicionarPorPosicao(int posicao, Conta cc) {
+		if (!verificaPosicao(posicao)) {
+
+			throw new IllegalArgumentException("posição inválida!");
+		}
+
+		for (int cont = this.total - 1; cont >= posicao; cont--) {
+
+			this.conta[cont + 1] = this.conta[cont];
+
+		}
+
+		this.conta[posicao] = cc;
+
+		this.total++;
+
+	}
+
 	public void listar() {
 
 		System.out.println(Arrays.toString(this.conta));
 
 	}
-	
+
 	public Conta procurarPosicao(int p) {
+
+		if (!verificaPosicao(p)) {
+
+			throw new IllegalArgumentException("posição inválida!");
+		}
+
 		return this.conta[p];
+	}
+
+	public Conta procurarNome(String nome) {
+		Conta c = null;
+
+		for (int cont = 0; cont < this.total; cont++) {
+			// utilizado o método Equals da classe String para comparar Strings
+			if (nome.equalsIgnoreCase(this.conta[cont].getNome())) {
+				c = this.conta[cont];
+			}
+
+		}
+
+		return c;
+	}
+
+	private boolean verificaPosicao(int posicao) {
+		return posicao >= 0 && posicao <= this.total;
 	}
 
 }
--- Questao3/src/br/edu/ifba/principal/Principal.java
+++ Questao3/src/br/edu/ifba/principal/Principal.java
@@ -7,16 +7,27 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Conta c = new Conta("Fulano", 20);
 		Conta c1 = new Conta("Beltrano", 10.5);
-		
+		Conta c2 = new Conta("Sicrano", 15.5);
+		Conta c3 = new Conta("Coisinha", 0.5);
 		CadastroConta cc = new CadastroConta();
 		cc.adicionar(c);
 		cc.adicionar(c1);
+		cc.adicionar(c2);
+		cc.adicionarPorPosicao(1, c3);
 		cc.listar();
-		System.out.println(cc.procurarPosicao(1));
-		
+
+		System.out.println(cc.procurarPosicao(2));
+		Conta conta = cc.procurarNome("hulano");
+		if (conta == null) {
+			System.out.println("Conta não encontrada");
+		} else {
+			System.out.println(conta);
+		}
+
+		System.out.println();
 
 	}
 
