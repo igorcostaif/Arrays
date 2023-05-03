db5604d3bff761d8f1949054928d1590f83a8e0c
IgorCosta <94564085+igorcostaif@users.noreply.github.com>
2023-05-03 10:46:46 -0300

    Add files via upload

---
+++ Empresa/src/br/edu/ifba/cadastros/CadastrosFuncionarios.java
@@ -0,0 +1,25 @@
+package br.edu.ifba.cadastros;
+
+import br.edu.ifba.basicas.Funcionario;
+
+public class CadastrosFuncionarios {
+	
+	Funcionario[] func = new Funcionario[5];
+	int total = 0;
+	
+	public void adicionar(Funcionario f) {
+		
+	}
+	
+	public String listar() {
+		return "";
+	}
+	
+	public Funcionario procurar(String nome) {
+		return null;
+	}
+	
+	public void remover(int posicao) {
+		
+	}
+}
